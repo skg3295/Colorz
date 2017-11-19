be1a188258cef0d8836596b53d4f5ce82f11cd8f
Deepti Gohil <madhugohil@madhus-mbp.fios-router.home>
2017-11-19 12:06:50 -0500

    initial project

---
+++ Colorz/Colorz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Colorz
+//
+//  Created by Madhu Gohil on 11/19/17.
+//  Copyright © 2017 Colorz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
