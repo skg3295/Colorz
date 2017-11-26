7f2a84214e51dab305bee071b689cd3dcfb8bbcd
Leela I <unicornGirl@anushas-air.fios-router.home>
2017-11-26 12:06:26 -0500

    I did the screen with the chroma color picker. It also has our colorz logo.

--- Colorz/Colorz/ViewController.swift
+++ Colorz/Colorz/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let neatColorPicker = ChromaColorPicker(frame: CGRect(x: view.frame.width/9, y: view.frame.height/3, width: 300, height: 300))
+        view.addSubview(neatColorPicker)
+        
+        neatColorPicker.padding = 0
+        neatColorPicker.hexLabel.isHidden = true
+        
+        neatColorPicker.layout()
     }
 
     override func didReceiveMemoryWarning() {
