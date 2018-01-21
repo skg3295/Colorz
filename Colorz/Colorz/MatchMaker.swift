ec1ecec1328bfc4ea0d11515f05bd14c4e043e45
Deepti Gohil <madhugohil@madhus-mbp.fios-router.home>
2018-01-21 12:12:06 -0500

    Made Match Screen Work

--- Colorz/Colorz/MatchMaker.swift
+++ Colorz/Colorz/MatchMaker.swift
@@ -34,41 +34,46 @@ class MatchMaker {
         
         print("startHLS.hue = \(String(startHSL.hue))")
         
-        let hue1 = abs((startHSL.hue + (1/3)) - 1)
-        let hue2 = abs((startHSL.hue + (2/3)) - 1)
+        let hue1 = abs((startHSL.hue + (1.0/3.0)) - 1.0)
+        let hue2 = abs((startHSL.hue + (2.0/3.0)) - 1.0)
         
         let endHSL1 = HSL()
         let endHSL2 = HSL()
         
         endHSL1.hue = hue1
         endHSL1.saturation = startHSL.saturation
-        endHSL1.lightness = startHSL.saturation
+        endHSL1.lightness = startHSL.lightness
         
         endHSL2.hue = hue2
         endHSL2.saturation = startHSL.saturation
-        endHSL2.lightness = startHSL.saturation
+        endHSL2.lightness = startHSL.lightness
         
         
         return (endHSL1, endHSL2)
     }
     
     func findSplitComplementary(startHSL: HSL) -> (matchColor1: HSL, matrchColor2: HSL) {
+        print("INSIDE findSplitComplementary")
+        print("startHSL.hue = \(String(startHSL.hue))")
+        print("startHSL.saturation = \(String(startHSL.saturation))")
+        print("startHSL.lightness = \(String(startHSL.lightness))")
         
-        print("startHLS.hue = \(String(startHSL.hue))")
+        let hue1 = abs((startHSL.hue + (150.0/360.0)) - 1.0)
+        let hue2 = abs((startHSL.hue + (210.0/360.0)) - 1.0)
+        print("hue1 = \(String(hue1))")
+        print("hue2 = \(String(hue2))")
         
-        let hue1 = abs((startHSL.hue + (150/360)) - 1)
-        let hue2 = abs((startHSL.hue + (210/360)) - 1)
         
         let endHSL1 = HSL()
         let endHSL2 = HSL()
         
         endHSL1.hue = hue1
         endHSL1.saturation = startHSL.saturation
-        endHSL1.lightness = startHSL.saturation
+        endHSL1.lightness = startHSL.lightness
         
         endHSL2.hue = hue2
         endHSL2.saturation = startHSL.saturation
-        endHSL2.lightness = startHSL.saturation
+        endHSL2.lightness = startHSL.lightness
         
         
         return (endHSL1, endHSL2)
